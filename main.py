5b94de243da76f0be31527c3b0f373355997e2f1
garrett <glw011@latech.edu>
2024-07-02 19:12:45 -0500

    I lied, I'm adding a few more things, I'm sure I'll regret it but it is what it is. We're using git so might as well rollback to a previous build at least once. This build seems stable and has finished buttons on right side of the window.

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import sys
 import pygame as py
 import pygame.mouse
 
-from game.constants import BOARD_WIDTH, HEIGHT, RED, BLACK, SQUARE_SIZE, WINDOW_SIZE, BOARD_POS, X_OFFSET
+from game.constants import BOARD_WIDTH, HEIGHT, RED, BLACK, SQUARE_SIZE, WINDOW_SIZE, BOARD_POS, X_OFFSET, REDB
 from game.game import Game
 from game.bot import Bot
+
+py.init()
 
 # Frame rate constant for clock.tick()
 FPS = 60
@@ -16,7 +18,7 @@ WINDOW = py.display.set_mode(WINDOW_SIZE)
 # Surface which will be blit onto window for the board to drawn on
 BOARD_SURF = py.Surface((BOARD_WIDTH, HEIGHT))
 
-# Constants determining the position and size of the buttons relative to screen size
+# Button constants for their position/size/font/etc.
 BTN_WDT = 3*(X_OFFSET//4)
 BTN_HT = BTN_WDT//3
 
@@ -28,10 +30,28 @@ REV_POS = (R_BTN_X, BTN_Y)
 RESET_POS = (R_BTN_X, WINDOW_SIZE[1]//2 - BTN_SZE[1]//2)
 QUIT_POS = (R_BTN_X, WINDOW_SIZE[1] - BTN_Y - BTN_SZE[1])
 
+#BTN_SURF = py.Surface(BTN_SZE)
+BTN_FONT = py.font.Font(None, 22)
+BTN_FONT.underline = True
 
+rev_surf = py.Surface(BTN_SZE)
+reset_surf = py.Surface(BTN_SZE)
+quit_surf = py.Surface(BTN_SZE)
+
+rev_txt = BTN_FONT.render('Undo Last Move', True, (255, 255, 255))
+reset_txt = BTN_FONT.render('Reset Game', True, (255, 255, 255))
+quit_txt = BTN_FONT.render('Quit', True, (255, 255, 255))
+
+rev_txt_rect = rev_txt.get_rect(center=(rev_surf.get_width() // 2, rev_surf.get_height() // 2))
+reset_txt_rect = reset_txt.get_rect(center=(reset_surf.get_width() // 2, reset_surf.get_height() // 2))
+quit_txt_rect = quit_txt.get_rect(center=(quit_surf.get_width() // 2, quit_surf.get_height() // 2))
+
+rev_btn_rect = py.Rect((REV_POS, BTN_SZE))
+reset_btn_rect = py.Rect((RESET_POS, BTN_SZE))
+quit_btn_rect = py.Rect((QUIT_POS, BTN_SZE))
 
 # Set window caption
-py.display.set_caption("Checkers v1.00")
+py.display.set_caption("Checkers v1.05")
 
 BOT = True
 BOT_COLOR = BLACK
@@ -94,20 +114,46 @@ def main():
                         game.select(row, col)
 
                 if REV_POS[0] <= tap_pos[0] <= REV_POS[0] + BTN_WDT and REV_POS[1] <= tap_pos[1] <= REV_POS[1] + BTN_HT:
+                    rev_btn_clk = py.draw.rect(WINDOW, REDB, (REV_POS, BTN_SZE))
+                    py.display.update(rev_btn_clk)
                     game.reverse_turn()
                     game.update()
                 if RESET_POS[0] <= tap_pos[0] <= RESET_POS[0] + BTN_WDT and RESET_POS[1] <= tap_pos[1] <= RESET_POS[1] + BTN_HT:
+                    reset_btn_clk = py.draw.rect(WINDOW, REDB, (RESET_POS, BTN_SZE))
+                    py.display.update(reset_btn_clk)
                     game.reset()
                 if QUIT_POS[0] <= tap_pos[0] <= QUIT_POS[0] + BTN_WDT and QUIT_POS[1] <= tap_pos[1] <= QUIT_POS[1] + BTN_HT:
+                    quit_btn_clk = py.draw.rect(WINDOW, REDB, (QUIT_POS, BTN_SZE))
+                    py.display.update(quit_btn_clk)
                     run = False
+                py.time.delay(75)
 
-        py.draw.rect(WINDOW, RED, (REV_POS, BTN_SZE))
-        py.draw.rect(WINDOW, RED, (RESET_POS, BTN_SZE))
-        py.draw.rect(WINDOW, RED, (QUIT_POS, BTN_SZE))
+        py.draw.rect(rev_surf, RED, ((0, 0), BTN_SZE))
+        py.draw.rect(reset_surf, RED, ((0, 0), BTN_SZE))
+        py.draw.rect(quit_surf, RED, ((0, 0), BTN_SZE))
+
+        rev_surf.blit(rev_txt, rev_txt_rect)
+        reset_surf.blit(reset_txt, reset_txt_rect)
+        quit_surf.blit(quit_txt, quit_txt_rect)
+
+        WINDOW.blit(rev_surf, REV_POS)
+        WINDOW.blit(reset_surf, RESET_POS)
+        WINDOW.blit(quit_surf, QUIT_POS)
+
         game.update()
+
         WINDOW.blit(BOARD_SURF, BOARD_POS)
+
+        py.display.update()
     py.quit()
     sys.exit()
 
+
+def intro():
+    pass
+
+
+def game_won():
+    pass
 
 main()
